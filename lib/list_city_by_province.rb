79d5325bdd6e3d841304be23496f37b2e1c482ff
Fabio Pozzi <fabio.pozzi@anche.no>
2011-03-09 16:40:02 +0100

    Completata feature: ora lo script produce in uscita un file chiamato Italy.txt contenente tutte le citta' d'Italia suddivise gerarchicamente per regione,poi per provincia. En: feature completed: the script produces an output file called Italy.txt containing all the italian cities ordered hierarchically by region, then by province.

--- lib/list_city_by_province.rb
+++ lib/list_city_by_province.rb
@@ -4,6 +4,7 @@ ITALY = 3175395
 PATH = "/home/wintermute/code/"
 STATE_FILE = PATH + "IT.txt"
 REGIONS_FILE = PATH + "regioni.txt"
+OUTPUT_FILE = PATH + "Italy.txt"
 
 # Il metodo apre il file contenente tutti i punti di interesse dello stato
 # e cerca un match, restituendo un array con tutti i risultati del grep
@@ -18,25 +19,51 @@ def greppa_stato( search_string )
 end
 
 def crea_file_stato_online
-  f = File.open("/home/wintermute/code/Italy.txt","w+")
-  puts "File Italy aperto"
+  f = File.open( OUTPUT_FILE ,"w+")
+  puts "Output file aperto"
 
   regions = Geonames::WebService.children_search( ITALY )
   puts "Elenco delle regioni ottenuto"
-  #File.open("/home/wintermute/code/tmp","w+").puts(regions)
   regions.each do |region|
     f.puts("*******   Regione: " + region.name + " geoname id: " + region.geoname_id)
     provinces = Geonames::WebService.children_search( region.geoname_id.to_i )
-    #pp provinces
     provinces.each do |province|
-      puts "Sto smandruppando la " + province.name
-      infos = greppa_stato(province.geoname_id) # greppo info sulla provincia da IT.txt cosi da avere tutti i campi (il webservice non li ritorna tutti)
-      #pp infos
-      f.puts( infos )
+      puts "Sto elaborando " + province.name
+      infos = greppa_stato(province.geoname_id)
+      # greppo info sulla provincia da IT.txt cosi da avere tutti i campi (il webservice non li ritorna tutti)
+      province_infos = infos.first.split(/\t/)
+      name = province_infos[1]
+      name_abbr = province_infos[11]
+      f.puts("****** Provincia: " + name + "  " + name_abbr)
+      cities = Geonames::WebService.children_search( province.geoname_id.to_i )
+      cities.each do |city|
+        f.puts( "Nome: " + city.name )
+      end
     end
   end
 end
 
+def crea_file_example
+  f = File.open( OUTPUT_FILE ,"w+")
+  puts "Output file aperto"
+  regions = Geonames::WebService.children_search( ITALY )
+  puts "Elenco delle regioni ottenuto"
+  region = regions.first
+  f.puts("*******   Regione: " + region.name + " geoname id: " + region.geoname_id)
+  provinces = Geonames::WebService.children_search( region.geoname_id.to_i )
+  province = provinces.first
+  puts "Sto elaborando " + province.name
+  infos = greppa_stato(province.geoname_id) # greppo info sulla provincia da IT.txt cosi da avere tutti i campi (il webservice non li ritorna tutti)
+  province_infos = infos.first.split(/\t/)
+  name = province_infos[1]
+  name_abbr = province_infos[11] # posizione del campo contenente la sigla della provincia
+  f.puts( name + "  " + name_abbr)
+  cities = Geonames::WebService.children_search( province.geoname_id.to_i )
+
+  cities.each do |city|
+    f.puts( "Nome: " + city.name )
+  end
+end
 # TO BE COMPLETED
 def crea_file_usando_file_stato
   region_match = "[0-9]+\\sRegione "
