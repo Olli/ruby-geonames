56e44cf44d48a6d28bcc9c0b3ebc7ece4919b9dd
Fabio Pozzi <pozzi.fabio@gmail.com>
2011-03-11 13:49:57 +0100

    modifiche per lavorare su england

--- lib/list_city_by_province.rb
+++ lib/list_city_by_province.rb
@@ -1,10 +1,11 @@
 require 'geonames'
 require 'pp'
 ITALY = 3175395
+ENGLAND = 2635167
 PATH = "/home/wintermute/code/"
-STATE_FILE = PATH + "IT.txt"
-REGIONS_FILE = PATH + "regioni.txt"
-OUTPUT_FILE = PATH + "Italy.txt"
+#STATE_FILE = PATH + "IT.txt"
+STATE_FILE = PATH + "GB.txt"
+OUTPUT_FILE = PATH + "Britain.txt"
 
 # Il metodo apre il file contenente tutti i punti di interesse dello stato
 # e cerca un match, restituendo un array con tutti i risultati del grep
@@ -22,19 +23,28 @@ def crea_file_stato_online
   f = File.open( OUTPUT_FILE ,"w+")
   puts "Output file aperto"
 
-  regions = Geonames::WebService.children_search( ITALY )
+  #regions = Geonames::WebService.children_search( ITALY )
+  regions = Geonames::WebService.children_search( ENGLAND )
   puts "Elenco delle regioni ottenuto"
   regions.each do |region|
     f.puts("*******Regione:\t" + region.name + "\t" + region.geoname_id)
     provinces = Geonames::WebService.children_search( region.geoname_id.to_i )
+    #f.puts(provinces)
     provinces.each do |province|
-      puts "Sto elaborando " + province.name
-      infos = greppa_stato(province.geoname_id)
-      # greppo info sulla provincia da IT.txt cosi da avere tutti i campi (il webservice non li ritorna tutti)
-      province_infos = infos.first.split(/\t/)
-      name = province_infos[1]
-      name_abbr = province_infos[11]
-      f.puts("******Provincia:\t" + name + "\t" + name_abbr)
+      puts "Sto elaborando " + province.name + "\t" + province.geoname_id
+
+      #infos = greppa_stato(province.geoname_id)
+      #f.puts infos if infos
+      # greppo info sulla provincia cosi da avere tutti i campi (il webservice non li ritorna tutti)
+      #if infos
+      #  province_infos = infos.first.split(/\t/)
+      #  name = province_infos[1]
+      #  name_abbr = province_infos[11]
+      #  f.puts("******Provincia:\t" + name + "\t" + name_abbr)
+      #else
+        f.puts("******Provincia:\t" + province.name)
+        f.puts(province)
+      #end
       cities = Geonames::WebService.children_search( province.geoname_id.to_i )
       cities.each do |city|
         f.puts( "Nome:\t" + city.name )
